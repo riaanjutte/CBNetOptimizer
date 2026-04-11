3402762b5b66002291d74082dc9e21b7f73e54fb
riaanjutte <riaanjutte@gmail.com>
2026-04-11 18:53:24 +0100

    Delete Run-CBNetOptimizer.bat

--- Run-CBNetOptimizer.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -NoExit -ExecutionPolicy Bypass -File ""%~dp0CBNetOptimizer.ps1""'"
